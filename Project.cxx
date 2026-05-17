a347233b363f40b2927c7e88ec4a76741d50e28f
faiz <faizathaaillah2007@gmail.com>
2026-05-17 14:31:29 +0700

    Menambahkan fitur tambah booking

--- Project.cxx
+++ Project.cxx
@@ -5,7 +5,8 @@
 
 using namespace std;
 
-struct Rental {
+struct Rental
+{
     int id;
     char nama[50];
     int nomorMejaPS;
@@ -16,11 +17,63 @@ struct Rental {
 
 Rental *head = NULL;
 
-int main() {
+void tambahData()
+{
+
+    system("clear");
+
+    Rental *baru = new Rental;
+
+    cout << "===== TAMBAH BOOKING =====\n";
+
+    cout << "Masukkan ID Booking     : ";
+    cin >> baru->id;
+    cin.ignore();
+
+    cout << "Masukkan Nama           : ";
+    cin.getline(baru->nama, 50);
+
+    cout << "Nomor Meja PS           : ";
+    cin >> baru->nomorMejaPS;
+
+    cout << "Jam Main                : ";
+    cin >> baru->jamMain;
+
+    baru->harga = baru->jamMain * 5000;
+
+    baru->next = NULL;
+
+    if (head == NULL)
+    {
+        head = baru;
+    }
+
+    else
+    {
+
+        Rental *bantu = head;
+
+        while (bantu->next != NULL)
+        {
+            bantu = bantu->next;
+        }
+
+        bantu->next = baru;
+    }
+
+    cout << "\nBooking berhasil ditambahkan!\n";
+
+    cin.ignore();
+    cin.get();
+}
+
+int main()
+{
 
     int pilih;
 
-    do {
+    do
+    {
 
         system("clear");
 
@@ -39,18 +92,19 @@ int main() {
         cout << "\nPilih Menu : ";
         cin >> pilih;
 
-        switch (pilih) {
+        switch (pilih)
+        {
 
-            case 1:
-                cout << "\nMenu tambah booking";
-                break;
+        case 1:
+            tambahData();
+            break;
 
-            case 8:
-                cout << "\nProgram selesai\n";
-                break;
+        case 8:
+            cout << "\nProgram selesai\n";
+            break;
 
-            default:
-                cout << "\nMenu tidak tersedia!";
+        default:
+            cout << "\nMenu tidak tersedia!";
         }
 
         cin.ignore();
