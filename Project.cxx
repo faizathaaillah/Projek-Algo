894758f942550dc6a328b48961a05da67182ce72
faiz <faizathaaillah2007@gmail.com>
2026-05-17 09:44:19 +0700

    Membuat menu utama rental PS

---
+++ Project.cxx
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+
+    int pilih;
+
+    do {
+
+        system("clear");
+
+        cout << "===================================";
+        cout << "\n   SISTEM MANAJEMEN RENTAL PS";
+        cout << "\n===================================";
+        cout << "\n1. Tambah Booking";
+        cout << "\n2. Lihat Data Rental";
+        cout << "\n3. Cari Pelanggan";
+        cout << "\n4. Ubah Data Rental";
+        cout << "\n5. Hapus Booking";
+        cout << "\n6. Urutkan Harga Rental";
+        cout << "\n7. Simpan Data";
+        cout << "\n8. Keluar";
+        cout << "\n===================================";
+        cout << "\nPilih Menu : ";
+        cin >> pilih;
+
+        switch (pilih) {
+
+            case 1:
+                cout << "\nMenu tambah booking";
+                break;
+
+            case 8:
+                cout << "\nProgram selesai\n";
+                break;
+
+            default:
+                cout << "\nMenu tidak tersedia!";
+        }
+
+        cin.ignore();
+        cin.get();
+
+    } while (pilih != 8);
+
+    return 0;
+}
